40f1e6e9bbf5627e4373eaa7e0a6cdbc3c7df954
Martin Marusak <martin.marusak@protonmail.com>
2024-12-05 22:09:21 +0100

    Gulp Prettier Dist

--- gulpfile.js
+++ gulpfile.js
@@ -6,48 +6,62 @@ const purgecss = require('gulp-purgecss');
 const terser = require('gulp-terser');
 const fileinclude = require('gulp-file-include');
 
+// Prettier for DIST
+async function prettierDist() {
+  console.log('Running Prettier on dist HTML...');
+  const prettier = (await import('gulp-prettier')).default; // Dynamically import gulp-prettier
+  return src('./dist/**/*.html')
+    .pipe(prettier())
+    .pipe(dest('./dist/'))
+    .on('end', () => console.log('Prettier finished formatting HTML.'));
+}
+
 // File include
 function includeHTML() {
-  return gulp
-    .src(['./src/**/*.html', '!./src/components/*.html'])
+  console.log('Running HTML include...');
+  return src(['./src/**/*.html', '!./src/components/*.html', '!./src/templates/*.html'])
     .pipe(
       fileinclude({
         prefix: '@@',
         basepath: '@file'
       })
     )
-    .pipe(gulp.dest('./dist/'));
+    .pipe(dest('./dist/'));
 }
 
-// Compile, prefix
+// Compile and prefix SCSS
 function compilescss() {
+  console.log('Compiling SCSS...');
   return src('src/scss/styles.scss').pipe(sass()).pipe(prefix('last 2 versions')).pipe(dest('src/css'));
 }
 
-// Purge CSS
+// Purge unused CSS
 function purgecssTask() {
-  return gulp
-    .src('./src/css/*.css')
+  console.log('Purging unused CSS...');
+  return src('./src/css/*.css')
     .pipe(
       purgecss({
         content: ['./src/**/*.html']
       })
     )
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(dest('./dist/css'));
 }
 
 // Minify JS
 function jsmin() {
+  console.log('Minifying JS...');
   return src('src/js/*.js').pipe(terser()).pipe(dest('dist/js'));
 }
 
-// Watch
+// Watch files
 function watchTask() {
-  watch('./src/**/*.html', includeHTML);
-  watch('src/scss/**/*.scss', compilescss, purgecssTask);
-  watch('src/css/*.css', purgecssTask);
-  watch('src/js/*.js', jsmin);
+  console.log('Starting watch task...');
+  watch('./src/**/*.html', series(includeHTML, prettierDist)); // Ensure Prettier runs after HTML is included
+  watch('src/scss/**/*.scss', series(compilescss, purgecssTask)); // Run SCSS compilation and purge in series
+  watch('src/css/*.css', purgecssTask); // Watch CSS for purging
+  watch('src/js/*.js', jsmin); // Watch JS for minification
 }
 
-// Mother of all tasks
-exports.default = series(includeHTML, compilescss, purgecssTask, jsmin, watchTask);
+// Export tasks
+exports.prettierDist = prettierDist;
+exports.default = series(includeHTML, compilescss, purgecssTask, jsmin, prettierDist, watchTask);
